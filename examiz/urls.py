92626b1a42d10f11c652497aad553716f523a52c
Divyam Bhagchandani <divyam17339@iiitd.ac.in>
2018-08-13 02:57:58 +0530

    Forget Password feature and some minor bugs

--- examiz/urls.py
+++ examiz/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+import django.contrib.auth
 urlpatterns = [
 	url(r'^', include('quiz.urls', namespace="quiz")),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url('^', include('django.contrib.auth.urls')),
 ]
